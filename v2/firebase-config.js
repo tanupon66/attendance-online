d85973fb68e04d7058dc5f4f5d1c7c9bf1768f7e
tanupon66 <tanupon66@gmail.com>
2026-06-05 11:24:11 +0700

    Add files via upload

---
+++ v2/firebase-config.js
@@ -0,0 +1,9 @@
+// ใส่ค่า Firebase จริงของคุณที่นี่
+window.firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT.firebasestorage.app",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
---
+++ v2/service-worker.js
@@ -0,0 +1,5 @@
+const CACHE='attendance-v2-20260605';
+const FILES=['./','./index.html','./styles.css','./app.js','./firebase-config.js','./manifest.webmanifest','./assets/icon.svg'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).catch(()=>{}));});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
+self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));});
